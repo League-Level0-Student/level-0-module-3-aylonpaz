6ab7b58f5caad49195ccadca09b402065ec26558
league <league@adminisorsimac3.attlocal.net>
2025-09-05 18:29:27 -0700

    AMERICAAAAAAAAA

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -6,14 +6,43 @@ import org.jointheleague.graphical.robot.Robot;
 public class WalkOfFame {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
-		
-		// 1. Set the X position of the robot so that it starts on the left. 
+
+		// 1. Set the X position of the robot so that it starts on the left.
 		// You also need to show the robot to see the result of this line.
-		rob.setX(200);
-		rob.setY(250);
+		rob.setX(100);
+		rob.setY(100);
+		rob.setSpeed(100);
+		rob.penDown();
 		// 2. Make the robot draw a star shape. Hint: angle=144.
-		for
-		rob.setAngle(144);
+		
+			for (int mtnDewBotBaja = 0; mtnDewBotBaja < 8; mtnDewBotBaja += 1) {
+				for (int mtnDewBot = 0; mtnDewBot < 5; mtnDewBot += 1) {
+					rob.move(30);
+					rob.turn(144);
+				}
+				if(mtnDewBotBaja<7) {
+					rob.penUp();
+				
+				rob.turn(90);
+				rob.move(100);
+				rob.turn(-90);
+				}
+				rob.penUp();
+				if(mtnDewBotBaja<7) {
+					rob.penDown();
+				}
+				rob.penUp();
+				rob.setAngle(180);
+				rob.move(100);
+				rob.penDown();
+				rob.setAngle(180);
+			}
+			
+			
+			
+			
+			
+		
 		// 3. Set the length of each line in the star to 30.
 
 		/** THE CHALLENGE: **/
